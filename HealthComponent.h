de0c8464a4a891b9835ac9676e064cee7728c9c6
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2023-11-15 10:34:35 +0500

    Update HealthComponent.h
    LOC is decreased

--- HealthComponent.h
+++ HealthComponent.h
@@ -1,11 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "HealthComponent.generated.h"
-
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class TOONTANKS_API UHealthComponent : public UActorComponent
