e1807f5bab2bc944c9e9d1402ae338f1797151ec
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2023-11-15 10:30:26 +0500

    Update BasePawn.h
    Moderate changes in Header Files

--- BasePawn.h
+++ BasePawn.h
@@ -1,11 +1,7 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
 #include "BasePawn.generated.h"
-
 UCLASS()
 class TOONTANKS_API ABasePawn : public APawn
 {
