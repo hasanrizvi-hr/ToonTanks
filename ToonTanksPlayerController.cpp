745bad47e01d91685290e6e91123ee69880a9be7
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2023-11-15 10:33:21 +0500

    Update ToonTanksPlayerController.cpp
    LOC is decreased

--- ToonTanksPlayerController.cpp
+++ ToonTanksPlayerController.cpp
@@ -1,9 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "ToonTanksPlayerController.h"
 #include "GameFramework/Pawn.h"
-
 
 void AToonTanksPlayerController::SetPlayerEnabledState(bool bPlayerEnabled)
 {
