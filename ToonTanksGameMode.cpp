d27031a5a2d70546d898bdd512af93ba7ef0329b
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2024-01-15 18:09:26 +0500

    Update ToonTanksGameMode.cpp

--- ToonTanksGameMode.cpp
+++ ToonTanksGameMode.cpp
@@ -1,12 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "ToonTanksGameMode.h"
 #include "Kismet/GameplayStatics.h"
 #include "Tank.h"
 #include "Tower.h"
 #include "ToonTanksPlayerController.h"
-
 
 void AToonTanksGameMode::ActorDied(AActor *DeadActor)
 {
@@ -29,21 +27,16 @@ void AToonTanksGameMode::ActorDied(AActor *DeadActor)
         }
     }
 }
-
 void AToonTanksGameMode::BeginPlay()
 {
     Super::BeginPlay();
-
     HandleGameStart();
 }
-
 void AToonTanksGameMode::HandleGameStart()
 {
     TargetTowers = GetTargetTowerCount();
-
     Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
     ToonTanksPlayerController =Cast<AToonTanksPlayerController>(UGameplayStatics::GetPlayerController(this, 0));
-
     StartGame();
 
     if (ToonTanksPlayerController)
