74592a3c74f3a7cb0fbc6a464fa85f0ed4450877
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2024-01-16 15:22:51 +0500

    Code Shortened with changes

--- Tank.cpp
+++ Tank.cpp
@@ -2,30 +2,24 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "Kismet/GameplayStatics.h"
-
+*****************************************************************************8
 ATank::ATank()
 {
     SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
     SpringArm->SetupAttachment(RootComponent);
-
     Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
     Camera->SetupAttachment(SpringArm);
 }
-
 void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
     Super::SetupPlayerInputComponent(PlayerInputComponent);
-
     PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
     PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
-
     PlayerInputComponent->BindAction(TEXT("FIRE"), IE_Pressed, this, &ATank::Fire);
 }
-
 void ATank::Tick(float DeltaTime)
 {
     Super::Tick(DeltaTime);
-
     if (TankPlayerController)
     {
         FHitResult HitResult;
@@ -33,12 +27,10 @@ void ATank::Tick(float DeltaTime)
             ECollisionChannel::ECC_Visibility,
             false,
             HitResult);
-        
          //DrawDebugSphere(GetWorld(), HitResult.ImpactPoint, 25.f, 12, FColor::Red, false, -1.f);
         RotateTurret(HitResult.ImpactPoint);
     }
 }
-
 void ATank::HandleDestruction()
 {
     Super::HandleDestruction();
@@ -46,24 +38,21 @@ void ATank::HandleDestruction()
     SetActorTickEnabled(false);
     bAlive = false;
 }
-
 void ATank::BeginPlay()
 {
     Super::BeginPlay();
     TankPlayerController = Cast<APlayerController>(GetController());
-
 }
-
 void ATank::Move(float Value)
 {
     FVector DeltaLocation = FVector::ZeroVector;
     DeltaLocation.X = Value * Speed * UGameplayStatics::GetWorldDeltaSeconds(this);
     AddActorLocalOffset(DeltaLocation, true);
 }
-
 void ATank::Turn(float Value)
 {
     FRotator DeltaRotation = FRotator::ZeroRotator;
     DeltaRotation.Yaw = Value * TurnRate * UGameplayStatics::GetWorldDeltaSeconds(this);
     AddActorLocalRotation(DeltaRotation, true);
 }
+***This is much shorter version of code compared to previous one***
