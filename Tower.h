faa9246f383c05de4a03293925f39218bda3d855
Hasan <97510148+hasanrizvi-hr@users.noreply.github.com>
2024-02-16 19:02:17 +0500

    Update Tower.h

--- Tower.h
+++ Tower.h
@@ -2,35 +2,24 @@
 #include "CoreMinimal.h"
 #include "BasePawn.h"
 #include "Tower.generated.h"
-/**
- * 
- */
 UCLASS()
 class TOONTANKS_API ATower : public ABasePawn
 {
 	GENERATED_BODY()
-
 public:
 	virtual void Tick(float DeltaTime) override;
-	
 	void HandleDestruction();
-
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
 private:
 	class ATank* Tank;
-
 	UPROPERTY(EditDefaultsOnly, Category = "Combat")
 	float FireRange = 700.f;
-
 	FTimerHandle FireRateTimerHandle;
 	float FireRate = 2.f;
-	void CheckFireCondition();
-
+	void CheckFireCondition();+
 	bool InFireRange();
 };
-
 /*the header files are not getting read by the file*/
 /*still working on it will update on that vey soon*/
